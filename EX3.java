0cfb114cd31720d4af2c99de78e224c33223c205
artuh21 <130799136+artuh21@users.noreply.github.com>
2023-09-15 00:58:00 -0300

    EX3.java
    Signed-off-by: artuh21 <130799136+artuh21@users.noreply.github.com>

--- EX3.java
+++ EX3.java
@@ -18,7 +18,7 @@ public class DAO {
 		int porta = 5432;
 		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
 		String username = "postgres";
-		String password = "NaoFicadeOtarioProcurando";
+		String password = "senha123";
 		boolean status = false;
 
 		try {
